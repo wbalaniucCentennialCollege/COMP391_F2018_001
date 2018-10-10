b841a3b9048994f4c1449508d35fbdd4fadaa1be
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-10-10 12:19:40 -0400

    Laser fire, Boundary and Asteroid

--- COMP391_F2018_001/Assets/Scripts/PlayerController.cs
+++ COMP391_F2018_001/Assets/Scripts/PlayerController.cs
@@ -2,22 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-    // Variable Declarations
-    public float speed = 5.0f;
+[System.Serializable]
+public class Boundary
+{
     public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour {
+    
+    // Variable Declarations
+    [Header("Movement Settings")]
+    public float speed = 5.0f;
+    // public float xMin, xMax, yMin, yMax;
+    public Boundary boundary;
+
+    [Header("Attack Settings")]
+    public GameObject laser;        // Laser prefab
+    public GameObject laserSpawn;   // Laser spawn location
+    public float fireRate = 0.5f;   // Firing speed
 
     private Rigidbody2D rBody;
+    private float myTime = 0.0f;    // fireRate counter
 
 	// Use this for initialization
 	void Start () {
         rBody = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per framep
+	// Update is called once per frame
 	void Update () {
-		
+        // Debug.Log(Time.deltaTime);
+        myTime += Time.deltaTime; // DeltaTime represents the amount of time elapsed since the last Update() call.
+                                  // Debug.Log(myTime);
+
+        if (Input.GetButton("Fire1") && myTime > fireRate)
+        {
+            Instantiate(laser, laserSpawn.transform.position, laserSpawn.transform.rotation);
+            myTime = 0.0f;
+        }
 	}
 
     // Fixed framerate update. Used mainly for physics. 
@@ -36,7 +58,7 @@ public class PlayerController : MonoBehaviour {
 
         // Restrict our player movement
         rBody.position = new Vector2(
-            Mathf.Clamp(rBody.position.x, xMin, xMax), // Restricts X to xMin and xMax
-            Mathf.Clamp(rBody.position.y, yMin, yMax)); // Restrict Y to yMin and yMax
+            Mathf.Clamp(rBody.position.x, boundary.xMin, boundary.xMax), // Restricts X to xMin and xMax
+            Mathf.Clamp(rBody.position.y, boundary.yMin, boundary.yMax)); // Restrict Y to yMin and yMax
     }
 }
